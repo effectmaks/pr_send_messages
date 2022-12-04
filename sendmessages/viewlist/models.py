c151eebd32e0c01aa72d1c34c1d4a88e9b25222f
Maksim Balonin <effectmaks@gmail.com>
2022-12-04 16:26:29 +0300

    add database TaskList

--- sendmessages/viewlist/models.py
+++ sendmessages/viewlist/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db.models import Model
+from django.db.models import CharField
 
-# Create your models here.
+
+class TaskList(Model):
+    """
+    Модель с задачами рассылки
+    """
+    name = CharField(max_length=100, default='')  # имя рассылки
+    text_message = CharField(max_length=2000, default='')  # текст рассылки
+    status = CharField(max_length=20, default='')  # статус выполнения
+    status_text = CharField(max_length=500, default='')  # текст к статусу
