c8bcef966886e0f0cd2e44797cbcd3066b443391
Maksim Balonin <effectmaks@gmail.com>
2022-12-25 16:00:53 +0300

    Edit the table 'Client'. Delete a field's name is Status ans status text. Add the field's name is utc.

--- sendmessages/client/models.py
+++ sendmessages/client/models.py
@@ -1,5 +1,5 @@
 from django.db.models import Model
-from django.db.models import CharField
+from django.db.models import CharField, IntegerField
 
 
 class Client(Model):
@@ -9,5 +9,4 @@ class Client(Model):
     phone = CharField(max_length=100, default='')  # номер телефона
     code = CharField(max_length=100, default='')  # код мобильного оператора
     tag = CharField(max_length=2000, default='')  # произвольная метка
-    status = CharField(max_length=20, default='')  # часовой пояс
-    status_text = CharField(max_length=500, default='')  # текст к статусу
+    utc = IntegerField(default=0)  # часовой пояс
