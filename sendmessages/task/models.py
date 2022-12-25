823b0c7171f7bf0b45007075bc74c64994bf7761
Maksim Balonin <effectmaks@gmail.com>
2022-12-25 14:36:24 +0300

    Create table's name is Message

--- sendmessages/task/models.py
+++ sendmessages/task/models.py
@@ -1,5 +1,6 @@
 from django.db.models import Model
-from django.db.models import CharField, DateTimeField
+from django.db.models import ForeignKey, CASCADE  # связь таблиц
+from django.db.models import CharField, DateTimeField, BooleanField
 
 
 class Task(Model):
@@ -12,3 +13,14 @@ class Task(Model):
     filter = CharField(max_length=100, default='')  # фильтр свойств клиентов
     status = CharField(max_length=20, default='')  # часовой пояс
     status_text = CharField(max_length=500, default='')  # текст к статусу
+
+
+class Message(Model):
+    """
+    Модель с созданными(отправленными) сообщениями
+    """
+    sentdatetime = DateTimeField()  # дата и время создания
+    status = BooleanField(max_length=20, default=False)  # статус отправлено
+    task = ForeignKey('Task', default=None, on_delete=CASCADE, related_name='tasks')  # id рассылки
+    client = ForeignKey('client.Client', default=None, on_delete=CASCADE, related_name='clients')  # id клиента
+
