cda1a7a71a1fcc459f42a062c19d24beb4d377bc
Maksim Balonin <effectmaks@gmail.com>
2023-01-08 21:21:19 +0300

    edit urls of clients

--- sendmessages/client/urls.py
+++ sendmessages/client/urls.py
@@ -4,6 +4,6 @@ from .views import ClientList, ClientUpdate
 
 
 urlpatterns = [
-   path('clients', ClientList.as_view(), name='clients'),
+   path('clients/', ClientList.as_view(), name='clients'),
    path('clients/<int:id>/', ClientUpdate.as_view(), name='client_update'),
 ]
